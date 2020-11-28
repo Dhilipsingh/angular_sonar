1aaf57854e286f9aa5f53570ce9588d0fbeff8ba
Dhilipsingh <45051048+Dhilipsingh@users.noreply.github.com>
2020-11-28 20:35:18 +0530

    Update sonar-project.properties.js

--- sonar-project.properties.js
+++ sonar-project.properties.js
@@ -6,7 +6,7 @@ sonarqubeScanner(
             'sonar.sources':  'src',
             'sonar.tests':  'src',
             'sonar.inclusions'  :  '**', // Entry point of your code
-            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
+            'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx,index.js,index.test.js',
             'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
             'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
         }
